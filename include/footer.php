e940ea3eb131748e4afa6b14254c5fc3b00eed2b
Robert <106394727+robertonaps@users.noreply.github.com>
2022-11-27 09:23:59 +0800

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,242 @@
+
+<section class="footer-background">
+
+    <div class="container-fluid">
+
+
+
+        <div class="footer-sep">
+
+            <div class="footer-flex">
+
+
+
+                <div class="Social-media">
+
+                    <h2 class="footer-sep-h">Follow on!</h2>
+
+                    <i class="bi bi-meta" onclick="mysocialfb()"></i>
+                    <i class="bi bi-youtube" onclick="yYoutube()"></i>
+                    <i class="bi bi-github"></i>
+
+
+
+                </div>
+
+                <div class="Support">
+
+                    <h4 class="footer-sep-h">Support</h4>
+
+                    <a href="#" class="a-list" data-bs-toggle="modal" data-bs-target="#contact">
+                        Contact
+                    </a>
+
+                    
+                    <div class="modal fade" id="contact" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="contact">Contact</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+
+
+                                    <form action="contact.php" method="post">
+
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlInput1" class="form-label">Name</label>
+                                            <input type="text" class="form-control" name="first_name" placeholder="Enter your Name" required>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                                            <input type="email" class="form-control" name="emails" placeholder="Name@example.com" required>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                                            <textarea class="form-control" type="text" name="messages" required></textarea>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button class="btn btn-primary" type="submit" name="submit">Send</button>
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+
+                                        </div>
+
+
+                                
+
+
+
+                                    </form>
+
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- location -->
+
+
+                    <a href="#" class="a-list" data-bs-toggle="modal" data-bs-target="#disclaimer">
+                        Disclaimer
+                    </a>
+
+
+                    <div class="modal fade" id="disclaimer" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="disclaimert">Disclaimer</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+
+                                    <p class="p-disclaimer">All the information on this website - www.nalapyt.com - is published in good faith and for general information purpose only. nalapyt does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (nalapyt), is strictly at your own risk. nalapyt will not be liable for any losses and/or damages in connection with the use of our website.<br>
+                                        From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone 'bad'.</p>
+
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- disclaimer -->
+                    <a href="#" class="a-list" data-bs-toggle="modal" data-bs-target="#faq">
+                        FAQ
+                    </a>
+
+
+                    <div class="modal fade" id="faq" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="faq">FAQ</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+
+                                    <span class="wl">1. <a href="#">How to Become a Supporter of Our YouTube Channel?</a></span><br>
+                                    <span class="wl">2. <a href="#">How do I send you a photo so you can be featured on our YouTube channel?</a></span>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- faq -->
+
+
+
+
+                </div>
+
+                <div class="NalapYt">
+
+                    <h4 class="footer-sep-h">NalapYt</h4>
+
+                    <a class="a-list" href="index.php">Home</a>
+
+                    <a href="#" class="a-list" data-bs-toggle="modal" data-bs-target="#about">
+                        About
+                    </a>
+
+                    <div class="modal fade" id="about" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="about">ABOUT</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+
+                                    <p class="p-about-media">
+                                        Nalapchannel is a gaming YouTuber who creates content about various games that other gamers will enjoy. Everything I shared about a game is free on all mobile devices. This game requires you to make friends or hang out and give good tips so players can enjoy it.
+                                    </p>
+                                    <br />
+                                    <p class="p-about-media">
+                                        I will keep posting more important posts on my Website for all of
+                                        you. Please give your support and love.
+                                    </p>
+                                    <span class="span-about-media">Thanks For Visiting my Site</span>
+
+
+
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+
+
+
+            </div>
+
+
+
+        </div>
+
+    </div>
+    </div>
+
+
+
+
+
+</section>
+
+<!--footertagcopyright-->
+
+<footer>
+    <div class="container-fluid">
+        <div class="foter-algn">
+            <p class="copyright-text">
+                Copyright &copy; 2022 All Rights Reserved by
+                <a>NalapYt</a>
+            </p>
+        </div>
+    </div>
+</footer>
+
+<!-- JavaScript -->
+<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+<?php
+	if (isset($_SESSION['msg'])) {
+		$message = $_SESSION['msg']; ?>
+
+	<script>
+	 alertify.set('notifier','position', 'top-center');
+ 	alertify.success('<?php echo $message; ?>');
+	</script>
+
+	<?php	unset($_SESSION['msg']);
+	}	?>
+	
+
+
+
+<!--cdn jquery-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
---
+++ include/header.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!---bootrap5-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" . />
+
+    <!--header fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@700&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+
+    <!-- paragraph fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tiro+Telugu&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@1,300&display=swap" rel="stylesheet" />
+
+    <!--icons-->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+
+    <!--css-->
+    <link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
+
+	<!-- Bootstrap theme -->
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css" />
+
+
+
+
+    <title>NALAPYT</title>
+</head>
+
+<body>
+
+    <!--navbar responsive-->
+    <nav class="navbar navbar-expand-sm">
+        <div class="container-fluid">
+            <a class="navbar-brand">
+                <img class="logo" src="css/image/logo.png" />
+            </a>
+        </div>
+    </nav>
+    
+
+    <section class="sec-cover">
+        <div class="ver-line">
+        </div>
+    </section>
+    <!--cover-->
